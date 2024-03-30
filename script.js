74eb074c05e15501967010a77b49e3218f50a996
alesdev <aleswebgit@gmail.com>
2024-03-30 11:35:29 +0100

    use data, change script.js to module

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+import { texts as INITIAL_TEXTS } from "./data.js";
+
 const $time = document.querySelector('time');
 const $paragraph = document.querySelector('#paragraph');
 const $input = document.querySelector('input');
 
 const INITIAL_TIME = 30;
-
-const TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.`;
 
 let words = [];
 let currentTime = INITIAL_TIME;
@@ -14,12 +14,12 @@ initEvents();
 
 function initGame() {
   $input.value = '';
-
-  words = TEXT.split(' ');
+  
+  words = INITIAL_TEXTS[Math.floor(Math.random() * INITIAL_TEXTS.length)].split(' ');
   currentTime = INITIAL_TIME;
   $time.textContent = currentTime;
 
-  $paragraph.innerHTML = words.map((word, index) => {
+  $paragraph.innerHTML = words.map(word => {
     const letters = word.split('');
 
     return `
