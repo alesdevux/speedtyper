6fe940683b2ff563851bff3285ff00eacc0993e4
alesdev <aleswebgit@gmail.com>
2024-03-31 01:07:14 +0100

    Add detect the language of the browser & updating the text

---
+++ detect-language.js
@@ -0,0 +1,42 @@
+import { webContentText } from "./data.js";
+import { initGame } from "./script.js";
+
+const language = navigator.language;
+const $ca = document.querySelector('#ca-ES');
+const $es = document.querySelector('#es-ES');
+const $en = document.querySelector('#en-US');
+const $languageOptions = [$ca, $es, $en];
+
+updateSelectedLenguage(language);
+initLenguageEvents();
+
+function updateSelectedLenguage(language) {
+  const $html = document.querySelector('html');
+  if (language !== $html.lang) $html.lang = language;
+
+  $languageOptions.forEach($option => $option.classList.remove('active'));
+
+  updateWebText(language);
+
+  if (language === 'ca-ES') return $ca.classList.add('active');
+  if (language === 'es-ES') return $es.classList.add('active');
+  return $en.classList.add('active');
+}
+
+function updateWebText(language) {
+  $languageOptions.forEach($option => {
+    const text = webContentText[language][$option.id];
+    $option.textContent = text;
+  });
+}
+
+function initLenguageEvents() {
+  $languageOptions.forEach($option => {
+    $option.addEventListener('click', () => {
+      const newLanguage = $option.id;
+
+      updateSelectedLenguage(newLanguage);
+      initGame();
+    });
+  });
+}
